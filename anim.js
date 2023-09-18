f4313059abcba3f387beb57a9277967b5d868db4
Ruhiu Edwin <edwin.nganga@roamtech.com>
2023-09-18 12:48:21 +0300

    Updaate contents and images to new content

--- anim.js
+++ anim.js
@@ -1,33 +1,30 @@
+const images = document.querySelectorAll(".anim");
 
-    const images = document.querySelectorAll('.anim');
-
-    observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if(entry.intersectionRatio > 0) {
-                entry.target.style.animation = 'anim1 2s ${entry.target.dataset.delay} forwards ease-out';
-            }
-            else{
-                entry.target.style.animation= 'none';
-            }
-        })
-    })
-
-
-images.forEach(image => {
-    observer.observe(image)
-})
-
-const section = document.querySelectorAll('section');
 observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if(entry.intersectionRatio > 0) {
-            entry.target.style.background = 'entry.target.dataset.bg';
-        }
-        else{
-            entry.target.style.background= 'none';
-        }
-    })
-})
-section.forEach(section => {
-    observer.observe(section)
-})
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation =
+        "anim1 0.5s ${entry.target.dataset.delay} forwards ease-out";
+    } else {
+      entry.target.style.animation = "none";
+    }
+  });
+});
+
+images.forEach((image) => {
+  observer.observe(image);
+});
+
+const section = document.querySelectorAll("section");
+observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.background = "entry.target.dataset.bg";
+    } else {
+      entry.target.style.background = "none";
+    }
+  });
+});
+section.forEach((section) => {
+  observer.observe(section);
+});
